3a00b52bdadbd629c267824ac6f3b773e89f7610
Yannick <yannickcruz02@gmail.com>
2025-10-26 15:12:26 -0300

    feat: image upscale implementation

--- __init__.py
+++ __init__.py
@@ -4,15 +4,15 @@ import comfy.model_management as model_management
 import comfy.samplers
 import comfy.utils
 import comfy.sd
-from PIL import Image, ImageOps
+from comfy_extras.nodes_upscale_model import ImageUpscaleWithModel
 
 class YSC_HighResFix:
 
     def __init__(self):
         self.device = model_management.get_torch_device()
+        self.image_scaler = ImageUpscaleWithModel()
 
     @classmethod
-
     def INPUT_TYPES(cls):
         return {
             "required": {
@@ -63,3 +63,30 @@ class YSC_HighResFix:
     RETURN_NAMES = ("LATENT")
     FUNCTION = "refine_upscale"
     CATEGORY = "image/upscaling"
+
+
+    def image_upscale(self, upscale_model, image, upscale_factor, upscale_method):
+        samples = image.movedim(-1,1)
+
+        width = round(samples.shape[3] * upscale_factor)
+        height = round(samples.shape[2] * upscale_factor)
+
+        samples = self.__imageScaler.upscale(upscale_model, image)[0].movedim(-1,1)
+
+        upscaled_width = round(samples.shape[3])
+        upscaled_height = round(samples.shape[2])
+
+        if upscaled_width > width or upscaled_height > height:
+            samples = comfy.utils.common_upscale(samples, width, height, upscale_method, "disabled")
+            
+        samples = samples.movedim(1,-1)
+        return (samples,)
+    
+# Registro do nó
+NODE_CLASS_MAPPINGS = {
+    "YSC_HighresFix": YSC_HighresFix
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "YSC_HighresFix": "Highres Fix (YSC)"
+}
